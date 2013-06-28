8aff35ae3457e695b9f2b0180997bdf278ce15d1
RomanDegtyar <kan-kendarat@yandex.ru>
2013-06-27 23:22:06 -0700

    Интерфейс сервера.

---
+++ Release/miniproject/client_server/SimpleServer.java
@@ -0,0 +1,25 @@
+package miniproject.client_server;
+import miniproject.client_server.exceptions.ServerException;
+
+public interface SimpleServer {
+    
+    // Отправляет сообщение message клиенту.
+    // Гененирует ServerException, если отправка невозможна.
+    public void send (String message, int id) throws ServerException;
+    
+    // Отправляет сообщение message всем клиентам.
+    // Гененирует ServerException, если отправка невозможна.
+    public void sendAll (String message) throws ServerException;
+    
+    // Подключение клиента к серверу.
+    // Гененирует ServerException, если сервер зарегистрировал максимальное число клиентов.
+    public void connect (SimpleClient client) throws ServerException;
+    
+    // Отключеие клиента от сервера, используя id клиента.
+    // Генерирует ClientException, если клиент с таким id не зарегистрирован.
+    public void disconnect (int id) throws ServerException;
+    
+    // Отключеие клиента от сервера, используя ссылку на клиента.
+    // Генерирует ClientException, если клиент с таким id не зарегистрирован.
+    public void disconnect (SimpleClient client) throws ServerException;
+}
