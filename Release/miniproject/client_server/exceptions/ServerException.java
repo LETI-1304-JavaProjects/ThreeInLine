5cdf11ffe54f7660e1f76118df6beb18d8e9320c
RomanDegtyar <kan-kendarat@yandex.ru>
2013-06-27 22:52:18 -0700

    Исключение (обработка ошибок сервера).

---
+++ Release/miniproject/client_server/exceptions/ServerException.java
@@ -0,0 +1,22 @@
+package miniproject.client_server.exceptions;
+
+/**
+ * Класс-исключение.
+ * Служит для сообщения об ошибках в методах класса LocalServer.
+ */
+public class ServerException extends Exception {
+    private String message;
+    
+    public ServerException () {
+        message = "ERROR";
+    }
+    
+    public ServerException (String s) {
+        message = s;
+    }
+    
+    // Возвращает строку с причиной ошибки.
+    public String what () {
+        return message;
+    }
+}
