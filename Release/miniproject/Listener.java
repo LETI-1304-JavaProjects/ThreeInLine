4432e91e76d512cb9f6b397786c8d1d4a73efb8b
RomanDegtyar <kan-kendarat@yandex.ru>
2013-06-27 22:46:06 -0700

    Обработчик событий мыши.

---
+++ Release/miniproject/Listener.java
@@ -0,0 +1,67 @@
+package miniproject;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
+
+/**
+ * Обработчик событий мыши.
+ * Работает с одной клеткой.
+ * Изменяет внешний вид клетки на игровом поле.
+ * Если был сделан ход, вызывает функцию обработки хода в классе Launcher.
+ */
+public class Listener implements MouseListener {
+    private ImageIcon icon1, icon2;   
+    private Element figure;      
+    private Launcher obj;   
+    
+    public Listener (Element f, Launcher perrent) {
+        figure = f;
+        icon1 = (ImageIcon) f.getIcon();
+        icon2 = new ImageIcon(String.valueOf(f.getType())+"1.png");
+        obj = perrent;
+    }
+    
+    // Устанавливает для фигуры соответствующие ее типу изображения.
+    // (обичное и более яркое, оторбажаемое при наведении мыши).
+    public void setIcons () {
+        icon1 = new ImageIcon(String.valueOf(figure.getType())+".png");
+        figure.setIcon(icon1);
+        icon2 = new ImageIcon(String.valueOf(figure.getType())+"1.png");
+    }
+    
+    public void mouseClicked(MouseEvent e){ 
+    
+    }
+
+    public void mousePressed(MouseEvent e){ 
+    
+    }
+    
+    // Сообщает основному игровому классу Launcher о том, 
+    // что была выбрана одна из клеток.
+    // Выполняется лишь тогда, когда игрок имеет право на ход.
+    public void mouseReleased(MouseEvent e){
+        if(obj.isEnable()) {
+            obj.OneFigureChanged(figure);
+        } 
+    }
+   
+    // Устанавливает яркое изображение.
+    // Вызывается при наведении мыши.
+    // Выполняется лишь тогда, когда игрок имеет право на ход.
+    public void mouseEntered(MouseEvent e){
+        if(obj.isEnable()) {
+            figure.setIcon(icon2);
+        }  
+    }
+    
+    // Устанавливает более темное изображение.
+    // Вызывается при выходе мыши из области объекта.
+    // Выполняется лишь тогда, когда игрок имеет право на ход.
+    public void mouseExited(MouseEvent e) {
+        if(obj.isEnable()) {
+            figure.setIcon(icon1);
+        }  
+    }
+}
